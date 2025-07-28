1648c6ff89eae801cebe02c8007f8acb2df88c29
Daniel Hu <dan@daniels-mbp.mynetworksettings.com>
2025-07-27 21:39:15 -0400

    escape chars

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -21,7 +21,7 @@ export default function Home() {
 
     <div className='relative w-full h-screen flex flex-col items-center justify-center'>
       <div className='absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <h1 className='font-bold text-5xl xs:text-6xl lg:text-8xl text-accent'>Hi, I'm Dan.</h1>
+        <h1 className='font-bold text-5xl xs:text-6xl lg:text-8xl text-accent'>Hi, I&apos;m Dan.</h1>
         {/* <p className='font-light text-foreground text-lg'>I'm a software engineer at GitHub Copilot.</p> */}
       </div>
     </div>
